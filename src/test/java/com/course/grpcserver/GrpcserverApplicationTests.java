735c525eb28d23c8ec4e36e8de79f645610f1ddc
timpamungkas <timotius.pamungkas@gmail.com>
2026-02-18 09:15:25 +0700

    first commit

---
+++ src/test/java/com/course/grpcserver/GrpcserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.grpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
