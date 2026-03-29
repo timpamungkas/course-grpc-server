77c03f3dbff575b5ce107bf70e38b4ca08196f8c
timpamungkas <timotius.pamungkas@gmail.com>
2026-03-29 21:18:21 +0700

    update armeria

--- src/main/java/com/course/grpcserver/GrpcserverApplication.java
+++ src/main/java/com/course/grpcserver/GrpcserverApplication.java
@@ -1,22 +1,10 @@
 package com.course.grpcserver;
 
-import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.course.central.proto.resiliency.ResiliencyMessage.ResiliencyRequest;
-import com.course.grpcserver.grpc.client.service.ClientResiliencyService;
-
-import io.grpc.Status;
-import io.grpc.StatusException;
-import io.grpc.StatusRuntimeException;
-import io.grpc.protobuf.StatusProto;
 import lombok.extern.slf4j.Slf4j;
 
 @EnableScheduling
@@ -28,151 +16,8 @@ public class GrpcserverApplication implements CommandLineRunner {
 		SpringApplication.run(GrpcserverApplication.class, args);
 	}
 
-	private ClientResiliencyService clientResiliencyService;
-
-	public GrpcserverApplication(@Autowired ClientResiliencyService clientResiliencyService) {
-		this.clientResiliencyService = clientResiliencyService;
-	}
-
-	private void printExceptionStatus(String caller, Throwable e) {
-		var status = Status.fromThrowable(e);
-		log.error("{} catch {}: {}", caller, e.getClass().getSimpleName(), status);
-
-		var statusDetail = StatusProto.fromThrowable(e);
-		if (statusDetail != null) {
-			log.error("{} statusDetail: status code={}, message={}", caller, statusDetail.getCode(),
-					statusDetail.getMessage());
-			statusDetail.getDetailsList()
-					.forEach(
-							detail -> log.error("  - {} error detail type={}, value={}",
-									caller, detail.getTypeUrl(), detail));
-		}
-	}
-
 	@Override
 	public void run(String... args) throws Exception {
-		TimeUnit.SECONDS.sleep(5);
-
-		demoUnaryResiliency();
-		// demoServerStreamingResiliency();
-		// demoClientStreamingResiliency();
-		// demoBidirectionalStreamingResiliency();
-	}
-
-	private void demoUnaryResiliency() {
-		try {
-			var minDelaySecond = 0;
-			var maxDelaySecond = 1;
-			var statusCodes = List.of(0);
-			var timeout = Duration.ofSeconds(5);
-
-			var request = ResiliencyRequest.newBuilder()
-					.setMinDelaySecond(minDelaySecond)
-					.setMaxDelaySecond(maxDelaySecond)
-					.addAllStatusCodes(statusCodes)
-					.build();
-
-			var res = clientResiliencyService.callUnaryResiliency(request, timeout);
-
-			log.info("unaryResiliency: {}", res.getDummyString());
-		} catch (StatusException | StatusRuntimeException e) {
-			printExceptionStatus("unaryResiliency", e);
-		} catch (Exception e) {
-			log.error("unaryResiliency catch exception: {}", e.getMessage(), e);
-		}
-	}
-
-	private void demoServerStreamingResiliency() {
-		try {
-			var minDelaySecond = 0;
-			var maxDelaySecond = 1;
-			var statusCodes = List.of(0);
-			var timeout = Duration.ofSeconds(17);
-
-			var request = ResiliencyRequest.newBuilder()
-					.setMinDelaySecond(minDelaySecond)
-					.setMaxDelaySecond(maxDelaySecond)
-					.addAllStatusCodes(statusCodes)
-					.build();
-
-			clientResiliencyService.callServerStreamingResiliency(request, timeout);
-		} catch (StatusException | StatusRuntimeException e) {
-			printExceptionStatus("serverStreamingResiliency", e);
-		} catch (Exception e) {
-			log.error("serverStreamingResiliency catch exception: {}", e.getMessage(), e);
-		}
-	}
-
-	private void demoClientStreamingResiliency() {
-		try {
-			var minDelaySecond = 0;
-			var maxDelaySecond = 1;
-			var statusCodes = List.of(0);
-			var timeout = Duration.ofSeconds(20);
-
-			var request1 = ResiliencyRequest.newBuilder()
-					.setMinDelaySecond(minDelaySecond)
-					.setMaxDelaySecond(maxDelaySecond)
-					.addAllStatusCodes(statusCodes)
-					.build();
-
-			var request2 = ResiliencyRequest.newBuilder()
-					.setMinDelaySecond(minDelaySecond)
-					.setMaxDelaySecond(maxDelaySecond)
-					.addAllStatusCodes(statusCodes)
-					.build();
-
-			var request3 = ResiliencyRequest.newBuilder()
-					.setMinDelaySecond(minDelaySecond)
-					.setMaxDelaySecond(maxDelaySecond)
-					.addAllStatusCodes(statusCodes)
-					.build();
-
-			var requests = List.of(request1, request2, request3);
-
-			var response = clientResiliencyService.callClientStreamingResiliency(requests, timeout);
-
-			log.info("clientStreamingResiliency: {}", response.getDummyString());
-		} catch (StatusException | StatusRuntimeException e) {
-			printExceptionStatus("clientStreamingResiliency", e);
-		} catch (Exception e) {
-			log.error("clientStreamingResiliency catch exception: {}", e.getMessage(), e);
-		}
-	}
-
-	private void demoBidirectionalStreamingResiliency() {
-		try {
-			var minDelaySecond = 0;
-			var maxDelaySecond = 1;
-			var statusCodes = List.of(0);
-			var timeout = Duration.ofSeconds(20);
-
-			var request1 = ResiliencyRequest.newBuilder()
-					.setMinDelaySecond(minDelaySecond)
-					.setMaxDelaySecond(maxDelaySecond)
-					.addAllStatusCodes(statusCodes)
-					.build();
-
-			var request2 = ResiliencyRequest.newBuilder()
-					.setMinDelaySecond(minDelaySecond)
-					.setMaxDelaySecond(maxDelaySecond)
-					.addAllStatusCodes(statusCodes)
-					.build();
-
-			var request3 = ResiliencyRequest.newBuilder()
-					.setMinDelaySecond(minDelaySecond)
-					.setMaxDelaySecond(maxDelaySecond)
-					.addAllStatusCodes(statusCodes)
-					.build();
-
-			var requests = List.of(request1, request2, request3);
-
-			clientResiliencyService.callBidirectionalStreamingResiliency(requests, timeout);
-		} catch (StatusException | StatusRuntimeException e) {
-			printExceptionStatus("bidirectionalResiliency", e);
-		} catch (Exception e) {
-			log.error("bidirectionalResiliency catch exception: {}", e.getMessage(), e);
-		}
 	}
 
 }
--- src/main/java/com/course/grpcserver/config/ArmeriaConfig.java
+++ src/main/java/com/course/grpcserver/config/ArmeriaConfig.java
@@ -1,9 +1,12 @@
 package com.course.grpcserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.course.grpcserver.grpc.server.BankServiceGrpcServer;
 import com.course.grpcserver.grpc.server.HelloServiceGrpcServer;
+import com.course.grpcserver.grpc.server.ResiliencyServiceGrpcServer;
 import com.linecorp.armeria.server.docs.DocService;
 import com.linecorp.armeria.server.grpc.GrpcService;
 import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
@@ -12,10 +15,15 @@ import com.linecorp.armeria.spring.ArmeriaServerConfigurator;
 public class ArmeriaConfig {
 
     @Bean
-    public ArmeriaServerConfigurator armeriaServerConfigurator(HelloServiceGrpcServer helloServiceGrpcServer) {
+    public ArmeriaServerConfigurator armeriaServerConfigurator(
+            @Autowired HelloServiceGrpcServer helloServiceGrpcServer,
+            @Autowired BankServiceGrpcServer bankServiceGrpcServer,
+            @Autowired ResiliencyServiceGrpcServer resiliencyServiceGrpcServer) {
         return builder -> {
             var grpcService = GrpcService.builder()
-                    .addService(helloServiceGrpcServer)
+                    .addService("/hello", helloServiceGrpcServer)
+                    .addService("/bank", bankServiceGrpcServer)
+                    .addService("/resiliency", resiliencyServiceGrpcServer)
                     .enableUnframedRequests(true)
                     .useBlockingTaskExecutor(true)
                     .enableHttpJsonTranscoding(true)
